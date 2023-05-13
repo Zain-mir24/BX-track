d81443c11491f7d3e2e590c5cc8d1bb1197fc50b
noobcoder24 <zainnaeemk10@gmail.com>
2023-05-13 20:20:16 +0500

    Changes

--- frontend/src/store/Slices/index.js
+++ frontend/src/store/Slices/index.js
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk ,createSelector} from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const API_BASE_URL = 'http://localhost:5000/Books';
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,8 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import { createBook, fetchBooks, updateBook, deleteBook } from './Slices/index';
-import booksReducer from "./Slices/index"
-import thunk from 'redux-thunk';
-const middleware = [...getDefaultMiddleware(), thunk];
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import booksReducer from "./Slices/index";
 export const store = configureStore({
   reducer: {
     // your other reducers go here
     books: booksReducer,
   },
- 
 });
-
-
